12b18f2852d4c3eca2d235dd21df55c884c89e3a
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-19 00:04:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name) {
+  drivers.push("Ralph");
+}
+function destructivelyPrependDriver(name) {
+  drivers.unshift("Bob");
+}
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift();
+}
+function appendDriver(name) {
+  randomName = drivers.slice();
+  randomName.push(name);
+  return randomName;
+}
+function prependDriver(name) {
+  randomName = drivers.slice();
+  randomName.unshift(name);
+  return randomName;
+}
+function removeLastDriver(name) {
+  randomName = drivers.slice();
+  randomName.pop(name);
+  return randomName;
+}
+function removeFirstDriver(name) {
+  randomName = drivers.slice();
+  randomName.shift(name);
+  return randomName;
+}
